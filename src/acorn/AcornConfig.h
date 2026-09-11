a21d7909ae8aea0dea990976d625b86e61f34cfe
Reza Bayati <mohamadrezabt@gmail.com>
2026-09-11 15:25:50 -0400

    acorn: public fork URL (blueoak-tech/acorn-powder) + shell lockfile for CI

--- src/acorn/AcornConfig.h
+++ src/acorn/AcornConfig.h
@@ -1,5 +1,5 @@
 #pragma once
 // Acorn build identity. Shown in the intro text so the GPL source offer is visible in-app.
-// TODO(D2): point at the public fork once the GitHub org/name is decided.
-#define ACORN_SOURCE_URL "https://github.com/acorn-ai/acorn-powder"
+// Public fork (GPL-3.0 source offer for the web build).
+#define ACORN_SOURCE_URL "https://github.com/blueoak-tech/acorn-powder"
 #define ACORN_BRIDGE_PROTOCOL 1
